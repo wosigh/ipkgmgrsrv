a9f8c1da87786095b7220f09bd0c09240f6e2b13
Ryan Hope <rmh3093@Redman.buffalo.rr.com>
2009-10-05 12:50:00 -0400

    move luna headers to include/liblunaservice, add webosinternals to headers to prepare for merger

--- include/ipkgmgr.h
+++ include/ipkgmgr.h
@@ -1,5 +1,6 @@
 /*==============================================================================
- Copyright (C) 2009  Ryan Hope <rmh3093@gmail.com>
+ Copyright (C) 2009 Ryan Hope <rmh3093@gmail.com>
+ Copyright (C) 2009 WebOS Internals <http://www.webos-internals.org/>
 
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
